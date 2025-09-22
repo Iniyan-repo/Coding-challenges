2d1a6d3a6d1cba2834c0e24d130e6ec88be82100
Iniyan-repo <iniyan193@yahoo.com>
2025-09-23 00:41:03 +0530

    evaluate the custom function

--- stl_reader.py
+++ stl_reader.py
@@ -15,9 +15,16 @@ solid COMMENT (once on top only)
 ...
 endsolids
 [[i,j,k][x1,y1,z1][x2,y2,z2][x3,y3,z3]]
-
+F = (x/3+(y**2)*(z**2)) +((y/3)+(z**2)*(x**2))+((z/3)+(x**2)*(y**2))
 '''
 from utilities import area_triangle,vector_scale,vector_dot,normalize_vector,centroid_triangle
+
+
+def solve_f(pt):
+    x,y,z = pt
+    #make sure divergence is 1
+    return [(x/3+(y**2)*(z**2)),((y/3)+(z**2)*(x**2)),((z/3)+(x**2)*(y**2))]
+    
 def read_stl(path):
     # read the data and return a string list
     with open(path,"r",encoding="UTF-8") as file:
@@ -39,13 +46,14 @@ def calculate_area_and_volume(path : str)->tuple:
     
     for i in range(1,len(data)-3,4):
         del_area = area_triangle(data[i],data[i+1],data[i+2])
-        del_volume = (vector_dot(normalize_vector(data[i-1]),centroid_triangle(data[i],data[i+1],data[i+2]))*del_area)/3
+        #del_volume = (vector_dot(normalize_vector(data[i-1]),centroid_triangle(data[i],data[i+1],data[i+2]))*del_area)/3
+        del_volume = (vector_dot(normalize_vector(data[i-1]),solve_f(centroid_triangle(data[i],data[i+1],data[i+2])))*del_area)
         surface_area = surface_area+del_area
         volume = volume + del_volume
     return(surface_area,volume)
   
 if __name__ == "__main__":
-    path = input("Enter the Stl Path\n")
+    path = input("Enter the Stl Path\n") or "Input/myTet 9.stl"
     area,volume = calculate_area_and_volume(path)
     print("Surface Area: ", area," squared unit")
     print("Volume: ", volume," cubic unit")
